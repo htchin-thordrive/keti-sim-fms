bd6b776339e8a98619c8e86963352c399cc2e36a
hwpark <hwpark@thordrive.ai>
2023-11-14 12:08:40 +0900

    Initial template written

--- src/main.py
+++ src/main.py
@@ -1,5 +1,30 @@
+class FMS:
+    def __init__(self):
+        self.node_set = None
+        self.nav_fusion = None
+        self.pose_estimator = None
+        self.is_inter = None
+        self.is_arrival = None
+
+        # parameters
+        self.pub_hz = 1.0
+        self.car_ids = ["thor01","thor02","thor03"]
+        self.route_ids = ["1an","2an","3an"]
+
+    
+    def checkVehicleNodeSet(self):
+        pass
+
+    def pubSpeedLimit(self):
+        pass
+
+    def pubNodeSet(self):
+        pass
+    
+    # subscribers
+
 def main():
-    print("Hello world!")
+    fms = FMS()
 
 if __name__ == "__main__":
     main()
